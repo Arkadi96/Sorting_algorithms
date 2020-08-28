f524d27051f059a44988fa6f571c5379e9e6d46c
Arkadi96 <69543920+Arkadi96@users.noreply.github.com>
2020-08-28 18:31:03 +0400

    Base class
    Base class added for optimizing project

---
+++ Sorting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting_Algorithms
+{
+    class Sorting
+    {
+        protected int arraySize;
+        protected int[] array;
+
+        public virtual void changeArray() { }
+
+        public void printArray()
+        {
+            for (int i = array.Length - arraySize; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + "\t");
+            }
+        }
+    }
+}
